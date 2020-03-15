c691102cd58ae1b382dcf9a7234a6f3a768e7734
MateoLeon <mateoleon127@gmail.com>
2020-03-15 17:00:09 -0500

    Paso 2: Se agrega test más básico para el número de elementos con solo un elemento (Rojo)

--- Procesamiento.py
+++ Procesamiento.py
@@ -2,3 +2,5 @@ class Procesamiento:
     def procesar(self,cadena):
         if cadena == "":
             return [0,0,0,0]
+
+
--- procesamiento_test.py
+++ procesamiento_test.py
@@ -1,7 +1,20 @@
 from unittest import TestCase
 
+'''
+Cadena vacía
+Mínimo
+Máximo
+Promedio
+Cuando solo nos envían un número
+Cuando nos envían dos números
+Cuando nos envían múltiples números separados por ,
+Número de elementos
+'''
+
 from Procesamiento import Procesamiento
 
 class ProcesamientoTest(TestCase):
     def test_cadenaVacia(self):
         self.assertEquals(Procesamiento().procesar(""),[0,0,0,0],"Cadena vacía")
+    def test_numeroDeElementosUnNumero(self):
+        self.assertEquals(Procesamiento().procesar("2"),1,"Un número")
