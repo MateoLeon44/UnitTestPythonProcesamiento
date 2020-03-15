14d2dfd7d13917339bc32c721fbbde5c72420e22
MateoLeon <mateoleon127@gmail.com>
2020-03-15 17:47:24 -0500

    Paso 6: Modificación de test de mínimo número y número de elementos para dos elementos (Verde)

--- Procesamiento.py
+++ Procesamiento.py
@@ -1,14 +1,13 @@
 class Procesamiento:
-    def procesar(self,cadena):
+    def procesar(self, cadena):
         if cadena == "":
-            return [0,0,0,0]
+            return [0, 0, 0, 0]
         elif cadena != "":
             if "," in cadena:
                 numeroElementos = 0
                 cadena = cadena.split(",")
+                numeroMinimo = min(cadena)
                 numeroElementos = len(cadena)
-                return [numeroElementos, 2, 0, 0]
+                return [numeroElementos, int(numeroMinimo), 0, 0]
             else:
-                return [1,int(cadena),0,0]
-
-
+                return [1, int(cadena), 0, 0]
