6a3ae44ced77a57e64854570c107337d7e4bbe93
MateoLeon <mateoleon127@gmail.com>
2020-03-15 17:24:14 -0500

    Paso 4: Creación de test de contar número de elementos con N elementos cubierto por test de paso 3 (Verde)

--- procesamiento_test.py
+++ procesamiento_test.py
@@ -19,4 +19,6 @@ class ProcesamientoTest(TestCase):
     def test_numeroDeElementosUnNumero(self):
         self.assertEquals(Procesamiento().procesar("2"),[1,0,0,0],"Un número")
     def test_numeroDeElementosDosNumeros(self):
-        self.assertEquals(Procesamiento().procesar("2,3"),[2,0,0,0],"Un número")
+        self.assertEquals(Procesamiento().procesar("2,3"),[2,0,0,0],"2 números")
+    def test_numeroDeElementosNNumeros(self):
+        self.assertEquals(Procesamiento().procesar("2,3,4,5"),[4,0,0,0], "N números")
